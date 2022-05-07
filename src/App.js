8a48aeb0b0608e83cdd2fce29bb801ef17308d85
Carson Roedl <CPROEDL@madisoncollege.edu>
2022-05-07 13:46:12 -0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -2,30 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return ( <
-    div className = "App" >
-    <
-    header className = "App-header" >
-    <
-    img src = {
-      logo
-    }
-    className = "App-logo"
-    alt = "logo" / >
-    <
-    h1 > Carson Roedl - Cloud
-    for Devs Project 04 < /h1> <
-    p >
-    Edit < code > src / App.js < /code> and save to reload. <
-    /p> <
-    a className = "App-link"
-    href = "https://reactjs.org"
-    target = "_blank"
-    rel = "noopener noreferrer" >
-    Learn React <
-    /a> <
-    /header> <
-    /div>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Carson Roedl - Cloud for Devs Project 04</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
